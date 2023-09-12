9c7abc1c8bf9ce45ee7336e406f670b433bb6340
MaxumH <hutyimax@gmail.com>
2023-09-11 23:22:16 -0400

    git commit -m "lab1redid"

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -4,21 +4,33 @@ import static java.lang.Math.*;
 import java.util.Scanner;
 
 public class Main {
+    public static void S(double a,double b,double x)
+    {
+        double s = 0;
+        s = Math.pow(x,3)*Math.pow(Math.tan(a),2)*Math.pow((x+b),2)+a/Math.sqrt(x+b);
+        System.out.println(" x = "+x+" b = "+b+" a = "+a+" s = "+s);
+    }
+    public static void Q(double a,double b,double x)
+    {
+        double e = 2.71828;
+        double Q = 0;
+        Q = b*Math.pow(x,2)*a/Math.pow(e,a*x)-1;
+        System.out.println(" x = "+x+" b = "+b+" a = "+a+" Q = "+Q);
+    }
     public static void main(String[] args) {
+        double a = 16.5;
+        double b = 3.4;
+        double x = 0.61;
         System.out.println("If you want output defalt values press 1");
         System.out.println("if you want to enter your values press 2\n");
-        Scanner s = new Scanner(System.in);
-        int q = s.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
         switch (q)
         {
             case 1:
-                double a = 16.5;
-                double b = 3.4;
-                double x = 0.61;
-                First obj = new First();
-                obj.s(a,b,x);
-                Q obj1 = new Q();
-                obj1.Q(a,b,x);
+
+                S(a,b,x);
+                Q(a,b,x);
             break;
             case 2:
                 System.out.println("Enter a,b,x");
@@ -28,10 +40,8 @@ public class Main {
                 b = w.nextDouble();
                 Scanner e = new Scanner(System.in);
                 x = e.nextDouble();
-                First obj2 = new First();
-                obj2.s(a,b,x);
-                Q obj3 = new Q();
-                obj3.Q(a,b,x);
+                S(a,b,x);
+                Q(a,b,x);
             break;
         }
     }
